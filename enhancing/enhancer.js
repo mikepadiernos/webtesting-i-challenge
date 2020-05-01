51d4d0848898e083785ea36681e4abbacfe70c34
Michael Padiernos <mike.padiernos@gmail.com>
2020-04-30 21:23:22 -0400

    [FEATURE] enhancer.js / Finished

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,22 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return item.enhancement <= 20
+    ? { ...item, enhancement: item.enhancement + 1 }
+    : { ...item};
 }
 
 function fail(item) {
-  return { ...item };
+  const {enhancement, durability} = item;
+  return enhancement > 16
+    ? { ...item, enhancement: enhancement - 1, durability: durability - 1 }
+    : enhancement >= 15
+      ? { ...item, durability: durability - 10}
+      : { ...item, durability: durability -5};
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
